1d409eddee840256354e8c9dea8744f52712d152
satchit24 <48960354+satchit24@users.noreply.github.com>
2020-10-25 11:03:45 +0530

    Add files via upload

--- CurveBuilder.java
+++ CurveBuilder.java
@@ -2,78 +2,73 @@ package processing;
 import java.security.Timestamp;
 import java.util.*;
 import processing.utility.*;
+import processing.BoardState;
 
 /**
 *
 * @author Satchit Desai
+* 
+* @reviewer Himanshu Jain
 */
+/* CurveBuilder Class to 
+ * convert curves in BoardObject
+ */
+
 
 public class CurveBuilder {
-	public static BoardObject drawCurve(ArrayList<Pixel> pixels, BoardObjectOperation newboardOp,
-			ObjectId newobjectId, Timestamp newtimestamp, UserId newuserId,
-			ArrayList<Pixel> prevPixelIntensities, Boolean isReset) {
-		
-		Iterator<Pixel> itr = pixels.iterator();
-
-		while(itr.hasNext()) {
-			
-			Pixel temp_pixel = (Pixel) itr.next();
-			
-			Intensity temp_intensity = temp_pixel.Intensity;
-			
-			Position temp_position = temp_pixel.Position;
-			
-			if(temp_intensity.r < 0) {
-				temp_intensity.r = 0;
-			}	else if(temp_intensity.r > 255) {
-				temp_intensity.r = 255;
-			}
-			
-			if(temp_intensity.g < 0) {
-				temp_intensity.g = 0;
-			}	else if(temp_intensity.g > 255) {
-				temp_intensity.g = 255;
-			}
-			
-			if(temp_intensity.b < 0) {
-				temp_intensity.b = 0;
-			}	else if(temp_intensity.b > 255) {
-				temp_intensity.b = 255;
-			}
-		}
-		
+	public static BoardObject drawCurve(
+			ArrayList<Pixel> pixels,
+			BoardObjectOperation newboardOp,
+			ObjectId newobjectId,
+			Timestamp newtimestamp,
+			UserId newuserId,
+			ArrayList<Pixel> prevPixelIntensity,
+			Boolean reset
+			) {
+				
 		BoardObject curveObj = new BoardObject();
+		
 		curveObj.pixels = pixels;
 		
+		//Update object of the new object
 		curveObj.objectid = new ObjectId();
+		
 		curveObj.objectid = newobjectId;
 		
+		//Update timestamp of the new object
 		curveObj.timestamp = new Timestamp();
+		
 		curveObj.timestamp = newtimestamp;
 		
+		//Update userid of the new object
 		curveObj.userid = new UserId();
+		
 		curveObj.userid = newuserId;
 		
-		//Map<Postion, Priority Queue<Pair<ObjectId,Timestamp>>(0, new ObjectComparator()> pixelToPQ;
-		//
-		Iterator<Pixel> itr2 = pixels.iterator();
+		//There is no previous intensity
+		//for a new object so it is null
+		curveObj.prevpixel = new prevPixelIntensities();
 		
-		while(itr.hasNext()) {
-			Pixel temp_pixel = (Pixel) itr2.next();
-			pixelToPQ.put(temp_pixel.Position, PriorityQueue<Pair<newobjectId, newtimestamp> >
-			(0, new ObjectComparator);
-		}
-		//Map<ObjectId, BoardObject> objIdToBoardObject;
-		objIdToBoardObject.put(newobjectId,curveObj);
+		curveObj.prevpixel = prevPixelIntensity;
 		
-		//Map<UserId,String> idToName;
-		String username = new getUsername();
-		idToName.put(curveObj.userid, username);
+		//Set reset flag as false
+		curveObj.resetFlag = new isReset();
 		
-		//added objects to shruti's maps
+		curveObj.resetFlag= reset;
 		
-		pushIntoStack(BoardObject curveObj);
-		//pushed to rakesh's stacks
+		//Insert BoardObject in the Map
+		insertObjectIntoMap(curveObj);
+		
+		//Push BoardObject in the stack
+		stackUtil(curveObj);
+		
 		return curveObj;
 	}
+	
+	private static void stackUtil(BoardObject newObj) {
+		
+		UndoRedo.pushIntoStack(newObj);
+	
+	}
+	
 }
